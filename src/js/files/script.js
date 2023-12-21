14538042c28a025b52443c2e7cc53424e83c61c4
Dima <uvarov3123@gmail.com>
2023-12-22 00:50:12 +0200

    add blog and popup

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -3,16 +3,28 @@ import { isMobile } from "./functions.js";
 // Підключення списку активних модулів
 import { flsModules } from "./modules.js";
 
-// window.addEventListener('load', pageLoad)
+window.addEventListener('load', pageLoad)
 
-// function pageLoad() {
-//     const htmlTag = document.documentElement
+function pageLoad() {
+    // const htmlTag = document.documentElement
 
-//     document.addEventListener('click', e => {
-//         const targetElement = e.target
+    // document.addEventListener('click', e => {
+    //     const targetElement = e.target
 
-//         if (targetElement.closest('.class')) {
-//             console.log('1');
-//         }
-//     })
-// }
+    //     if (targetElement.closest('.class')) {
+    //         console.log('1');
+    //     }
+    // })
+
+    document.addEventListener("afterPopupOpen", function (e) {
+        // Попап
+        const currentPopup = e.detail.popup;
+        const popup = currentPopup.targetOpen.element;
+        const inputServise = popup.querySelector('[data-servise]');
+        const currentButtonTitle = currentPopup.previousActiveElement.getAttribute('data-popup-title');
+
+        if (inputServise) {
+            inputServise.value = currentButtonTitle ? currentButtonTitle : '';
+        }
+        });
+}
